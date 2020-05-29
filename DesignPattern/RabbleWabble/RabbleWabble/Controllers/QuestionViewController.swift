be24a5414fb77f9fc273420eeaaa5ce795b32c7d
Bhavesh Tandel <bhavesh.tandel@digitas.com>
2020-05-29 13:35:46 +0530

    added delegate pattern

--- DesignPattern/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ DesignPattern/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
+public protocol QuestionViewControllerDelegate: class {
+    func questionViewController(_ viewController: QuestionViewController, didCancel questionGroup: QuestionGroup, at questionIndex: Int)
+    func questionViewController(_ viewController: QuestionViewController, didComplete questionGroup: QuestionGroup)
+}
+
 //View Controller
 public class QuestionViewController: UIViewController {
-    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionGroup: QuestionGroup! {
+        didSet {
+            navigationItem.title = questionGroup.title
+        }
+    }
+    
     public var questionIndex = 0
     
     public var correctCount = 0
     public var incorrectCount = 0
+    
+    public weak var delegate: QuestionViewControllerDelegate?
+    
+    private lazy var questionIndexItem: UIBarButtonItem = {
+        let item = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        item.tintColor = .black
+        navigationItem.rightBarButtonItem = item
+        return item
+    }()
     
     public var questionView: QuestionView! {
         guard isViewLoaded else { return nil }
@@ -23,8 +42,20 @@ public class QuestionViewController: UIViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        setupCancelButton()
         showQuestion()
     }
+    
+    private func setupCancelButton() {
+        let action = #selector(handleCancelPressed(sender:))
+        let image = UIImage(named: "ic_menu")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, landscapeImagePhone: nil, style: .plain, target: self, action: action)
+    }
+    
+    @objc private func handleCancelPressed(sender: UIBarButtonItem) {
+        delegate?.questionViewController(self, didCancel: questionGroup, at: questionIndex)
+    }
+    
     private func showQuestion() {
         let question = questionGroup.questions[questionIndex]
         questionView.answerLabel.text = question.answer
@@ -32,6 +63,7 @@ public class QuestionViewController: UIViewController {
         questionView.hintLabel.text = question.hint
         questionView.answerLabel.isHidden = true
         questionView.hintLabel.isHidden = true
+        questionIndexItem.title = "\(questionIndex + 1)/" + "\(questionGroup.questions.count)"
     }
     
     @IBAction func toggleAnswerLabels(_ sender: Any) {
@@ -54,10 +86,11 @@ public class QuestionViewController: UIViewController {
     private func showNextQuestion() {
         questionIndex += 1
         guard questionIndex < questionGroup.questions.count else {
-            // TODO: - Handle this...!
+            delegate?.questionViewController(self, didComplete: questionGroup)
             return
         }
         showQuestion()
     }
+    
 }
 
---
+++ DesignPattern/RabbleWabble/RabbleWabble/Views/QuestionGroupCell.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionGroupCell.swift
+//  RabbleWabble
+//
+//  Created by Bhavesh Tandel on 29/05/20.
+//  Copyright © 2020 Bhavesh Tandel. All rights reserved.
+//
+
+import UIKit
+
+class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+    
+}
